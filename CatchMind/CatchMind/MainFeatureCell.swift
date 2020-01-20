aaca2aa31f94cc8eec3cc2f3143768f34ee653ef
conyconydev <conyconydev@gmail.com>
2020-01-20 17:18:35 +0900

    add: IBOutlet 추가

--- CatchMind/CatchMind/MainFeatureCell.swift
+++ CatchMind/CatchMind/MainFeatureCell.swift
@@ -9,16 +9,11 @@
 import UIKit
 
 class MainFeatureCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    @IBOutlet weak var featureImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    
 }
