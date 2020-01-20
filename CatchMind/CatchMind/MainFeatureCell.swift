7fbbafe3e4c446e9ced088481f59eef495967bb5
conyconydev <conyconydev@gmail.com>
2020-01-20 17:14:49 +0900

    add: MainFeatureCell 파일 추가

---
+++ CatchMind/CatchMind/MainFeatureCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainFeatureCell.swift
+//  CatchMind
+//
+//  Created by kwangrae kim on 2020/01/20.
+//  Copyright © 2020 conyconydev. All rights reserved.
+//
+
+import UIKit
+
+class MainFeatureCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
