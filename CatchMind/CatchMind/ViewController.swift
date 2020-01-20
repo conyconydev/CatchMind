6cb8a3cf975014d986ee97352739737660d1e4c5
conyconydev <conyconydev@gmail.com>
2020-01-20 15:37:36 +0900

    add init Project

---
+++ CatchMind/CatchMind/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CatchMind
+//
+//  Created by kwangrae kim on 2020/01/20.
+//  Copyright © 2020 conyconydev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
