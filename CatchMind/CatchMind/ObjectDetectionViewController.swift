65c921ab38db0eadd73be9ba261e396317587395
conyconydev <conyconydev@gmail.com>
2020-01-21 15:21:12 +0900

    add ObjectDetectionViewController 추가

---
+++ CatchMind/CatchMind/ObjectDetectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ObjectDetectionViewController.swift
+//  CatchMind
+//
+//  Created by kwangrae kim on 2020/01/21.
+//  Copyright © 2020 conyconydev. All rights reserved.
+//
+
+import UIKit
+
+class ObjectDetectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
