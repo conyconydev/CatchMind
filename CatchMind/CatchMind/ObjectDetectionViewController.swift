da5bf640282ccdbb5fa1495ca3cfce5bab13dfb8
conyconydev <conyconydev@gmail.com>
2020-01-21 17:08:21 +0900

    add: 이미지를 앨범에서 가져와서 이미지 뷰에 보여주는 것까지 완성

--- CatchMind/CatchMind/ObjectDetectionViewController.swift
+++ CatchMind/CatchMind/ObjectDetectionViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class ObjectDetectionViewController: UIViewController {
-
+class ObjectDetectionViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    //MARK: IBOutlet
+    @IBOutlet weak var selectImageView: UIImageView!
+    
     //MARK: IBAction
     @IBAction func addPhoto(_ sender: UIBarButtonItem) {
         //
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let importFromAlbum = UIAlertAction(title: "앨범에서 가져오기", style: .default) { _ in
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .savedPhotosAlbum
+            picker.allowsEditing = true
+            
+            self.present(picker, animated: true, completion: nil)
+        }
+        let takePhoto = UIAlertAction(title: "카메라로 찍기", style: .default) { _ in
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .camera
+            picker.cameraCaptureMode = .photo
+            picker.allowsEditing = true
+            
+            self.present(picker, animated: true, completion: nil)
+        }
+        let cacel = UIAlertAction(title: "취소", style: .cancel) { _ in
+        }
+        
+        actionSheet.addAction(importFromAlbum)
+        actionSheet.addAction(takePhoto)
+        actionSheet.addAction(cacel)
+        
+        self.present(actionSheet,animated: true, completion: nil)
+    }
+    
+    //MARK: func
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let editedImage = info[.editedImage] as? UIImage{
+            self.selectImageView.image = editedImage
+            picker.dismiss(animated: true)
+        }
     }
     
     
