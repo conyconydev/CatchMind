520729aa5a6d7ad8a250978cc381ad42d7dfc3a1
conyconydev <conyconydev@gmail.com>
2020-01-21 14:40:48 +0900

    전반적인 부분 수정

--- CatchMind/CatchMind/SampleData.swift
+++ CatchMind/CatchMind/SampleData.swift
@@ -18,7 +18,10 @@ struct Sample {
 
 struct SampleData {
     
-    let samples = [Sample(title: "Phto" , description: "내용" , image: "이미지파일")];
+    let samples = [
+        Sample(title: "Photo Object Detection" , description: "불러온 이미지에 있는 사물 인식" , image: "ic_photo"),
+        Sample(title: "Real Time Object Detection" , description: "실시간으로 카메라에 보이는 사물 인식" , image: "ic_camera"),
+        Sample(title: "Facial Analysis" , description: "사람 얼굴로부터 나이, 성별, 감정 추측" , image: "ic_emotion")]
     
 }
 
