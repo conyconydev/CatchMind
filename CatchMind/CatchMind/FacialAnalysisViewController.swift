87a15dd04f607e86dda3da5aa3956491e90fd7c1
conyconydev <conyconydev@gmail.com>
2020-01-22 10:22:13 +0900

    add 사진에 블러 효과주는것 까지 해봄

---
+++ CatchMind/CatchMind/FacialAnalysisViewController.swift
@@ -0,0 +1,74 @@
+//
+//  FacialAnalysisViewController.swift
+//  CatchMind
+//
+//  Created by kwangrae kim on 2020/01/21.
+//  Copyright © 2020 conyconydev. All rights reserved.
+//
+
+import UIKit
+
+class FacialAnalysisViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    //MARK: value
+    
+    var selectdImage: UIImage? {
+        didSet {
+            self.blurredImageView.image = selectdImage
+            self.selectedImage.image = selectdImage
+        }
+    }
+    
+    //MARK: IBOutlet
+    @IBOutlet weak var blurredImageView: UIImageView!
+    @IBOutlet weak var selectedImage: UIImageView!
+    
+    //MARK: IBAction
+    @IBAction func addPhoto(_ sender: UIBarButtonItem) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let importFromAlbum = UIAlertAction(title: "앨범에서 가져오기", style: .default) { _ in
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .savedPhotosAlbum
+            picker.allowsEditing = true
+            
+            self.present(picker, animated: true, completion: nil)
+        }
+        let takePhoto = UIAlertAction(title: "카메라로 찍기", style: .default) { _ in
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .camera
+            picker.cameraCaptureMode = .photo
+            picker.allowsEditing = true
+            
+            self.present(picker, animated: true, completion: nil)
+        }
+        let cacel = UIAlertAction(title: "취소", style: .cancel) { _ in
+        }
+        
+        actionSheet.addAction(importFromAlbum)
+        actionSheet.addAction(takePhoto)
+        actionSheet.addAction(cacel)
+        
+        self.present(actionSheet,animated: true, completion: nil)
+    }
+    
+    //MARK: func
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let editedImage = info[.editedImage] as? UIImage{
+            picker.dismiss(animated: true)
+            self.selectdImage = editedImage
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
